0d830660f6fafd1aa3bb28dda90238757d6e6a86
sharoon kp <sharoonkp267@gmail.com>
2024-06-08 12:55:26 +0530

    refactor: bcryptjs type declaration installed

--- server/src/lib/envConfig.ts
+++ server/src/lib/envConfig.ts
@@ -4,5 +4,5 @@ config()
 
 export const PORT = process.env.PORT 
 export const MONGO_URI = process.env.MONGO_URI
-export const CLIENT_URL = process.env.CLIENT_URL
+export const CLIENT_URL = "https://wanderon-jcho.onrender.com"
 export const JWT_SECRET = process.env.JWT_SECRET
